e4e27c77b76a9822720a9c2d6b530634cd60ae69
Sunil Gogula <sunil.gogula@gmail.com>
2020-07-12 15:12:37 +0530

    Modified HelloController

--- src/main/java/com/example/CI_CD_AWS/HelloController.java
+++ src/main/java/com/example/CI_CD_AWS/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.CI_CD_AWS;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 	
+	@GetMapping
 	public String hello()
 	{
 		return "Hello Youtube";
